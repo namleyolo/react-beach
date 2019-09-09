7c93c6134330661059f4da3855db86d7059d25ff
namhoangle <happybear02042000@gmail.com>
2019-09-09 16:34:46 +0700

    nothing special

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import logo from "../images/gif/loading-arrow.gif" ;
-export default class Loading extends Component {
-    render() {
+
+
+
+export default function Loading() {
+
         return ( 
         <div className="text-center">
             <p> Loading ...</p>
             <img  src={logo} alt="Smiley face" height="100" width="100" />     
         </div>
         )
-    }
+    // }
 }
